84d7478fec79d8fa59eac0ecbdef71dc8c173287
AhmedBHameed <ahmedbazy@gmail.com>
2018-04-29 20:10:53 +0200

    Test GraphQL

--- http.py
+++ http.py
@@ -10,15 +10,16 @@ class Http:
 
     def __init__(self):
       print ('HTTP module initialized')
+    
     def get(self, page=''):
         return self.http.get(self.url + page).json()
         
-    def post(self, page, nfcid):
-        payload = payload || slef.query(nfcid)
-        return self.http.post(self.url + page, data=json.dumps(payload), headers=self.headers).json()
-    def query(nfcid):
+    def post(self, page, payload):
+          return self.http.post(self.url + page, data=json.dumps(self.query(nfcid)), headers=self.headers).json()
+    
+    def query(self, nfcid):
         return {
-            'variables': null,
+            'variables': None,
             'query': 'mutation { trakMyAss(nfcid: "' + nfcid + '") { id, freelancer_id, partner_id, log_date, login, logout, notes, ack { ok, message } } }'
         }
         
